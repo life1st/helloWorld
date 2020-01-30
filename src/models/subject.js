bf3d9c71d76cffc054ba883984806730bc3661a4
life1st <jiaoyangdev@gmail.com>
2020-01-30 20:17:39 +0800

    update: env windows.

--- src/models/subject.js
+++ src/models/subject.js
@@ -4,5 +4,17 @@ const mongoose = require('mongoose')
 const subjectSchema = new mongoose.Schema({
   id_douban: { type: Number, default: null },
   id_imdb: { type: Number, default: null },
-  
+  name: { type: String, required: true },
+  type: { 
+    type: String,
+    enum: ['movie', 'book', 'music'],
+  },
+  subtype: {
+    type: String,
+    enum: ['movie', 'tv', 'book', 'music']
+  }                                                                                                                                                                                                                                                                                                                                                                                                
 })
+
+const Subject = db.model('subject', subjectSchema)
+
+module.exports = { subject }
